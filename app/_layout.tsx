bef5aa64e6c7117c69c863757f757fee4ada7af5
jongsung <kizersoje2@naver.com>
2026-07-16 14:01:13 +0900

    add Sentry build

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -4,12 +4,27 @@ import { StatusBar } from 'expo-status-bar';
 import 'react-native-reanimated';
 
 import { useColorScheme } from '@/hooks/use-color-scheme';
+import * as Sentry from '@sentry/react-native';
+
+Sentry.init({
+  dsn: 'https://0a9e7c3097f5956555915f9f6b49ddda@o4511743181717504.ingest.us.sentry.io/4511743191023616',
+
+  // Adds more context data to events (IP address, cookies, user, etc.)
+  // For more information, visit: https://docs.sentry.io/platforms/react-native/data-management/data-collected/
+  sendDefaultPii: true,
+
+  // Enable Logs
+  enableLogs: false,
+
+  // uncomment the line below to enable Spotlight (https://spotlightjs.com)
+  // spotlight: __DEV__,
+});
 
 export const unstable_settings = {
   anchor: '(tabs)',
 };
 
-export default function RootLayout() {
+export default Sentry.wrap(function RootLayout() {
   const colorScheme = useColorScheme();
 
   return (
@@ -21,4 +36,4 @@ export default function RootLayout() {
       <StatusBar style="auto" />
     </ThemeProvider>
   );
-}
+});
--- metro.config.js
+++ metro.config.js
@@ -1,7 +1,9 @@
-const { getDefaultConfig } = require('expo/metro-config');
+const {
+  getSentryExpoConfig
+} = require("@sentry/react-native/metro");
 
 /** @type {import('expo/metro-config').MetroConfig} */
-const config = getDefaultConfig(__dirname);
+const config = getSentryExpoConfig(__dirname);
 
 // 🌟 Metro 번들러가 .wasm 파일을 자산(Asset) 또는 소스 확장자로 인식하도록 추가
 config.resolver.sourceExts.push('wasm'); 
